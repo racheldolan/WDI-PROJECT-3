0abda7b329817c39bf83680dda8f3706ec0cff5c
Natasha Ramburrun <r_natasha100@hotmail.com>
2018-06-23 15:52:22 +0100

    added files for the profile

--- src/config/routes.js
+++ src/config/routes.js
@@ -10,6 +10,21 @@ function Router($stateProvider, $urlRouterProvider){
       templateUrl: './views/recipes/show.html',
       controller: 'RecipesShowCtrl'
     })
+    // .state('usersShow', {
+    //   url: '/users/:id',
+    //   templateUrl: './views/users/show.html',
+    //   controller: 'UsersShowCtrl'
+    // })
+    // .state('usersNew', {
+    //   url: 'users/new',
+    //   templateUrl: './views/users/new.html',
+    //   controller: 'UsersNewCtrl'
+    // })
+    // .state('usersEdit', {
+    //   url: 'users/edit',
+    //   templateUrl: './views/users/edit.html',
+    //   controller: 'UsersNewCtrl'
+    // })
     .state('login', {
       url: '/login',
       templateUrl: './views/auth/login.html',
