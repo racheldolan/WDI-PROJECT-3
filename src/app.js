cdada6003f557497cd386d5897e87b50bfc96591
Natasha Ramburrun <r_natasha100@hotmail.com>
2018-06-23 08:38:18 +0100

    added ngMessages to the appjs and finished all required on error messaging

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import angular from 'angular';
 import '@uirouter/angularjs';
 import 'bulma';
+import 'angular-messages';
 
 import Router from './config/routes';
 
 import RecipesIndexCtrl from './controllers/recipes/index';
 import RecipesShowCtrl from './controllers/recipes/show';
 
-angular.module('fridgeAPI', ['ui.router'])
+angular.module('fridgeAPI', ['ui.router', 'ngMessages'])
   .config(Router)
   .controller('RecipesIndexCtrl', RecipesIndexCtrl)
   .controller('RecipesShowCtrl', RecipesShowCtrl);
