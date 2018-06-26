7bbb6981ec660972ef0cab0daa17b48e4b6a5d93
Linda Le <linchowmein@hotmail.com>
2018-06-26 17:00:56 +0100

    fixed merge conflicts

--- src/directives/filePicker.js
+++ src/directives/filePicker.js
@@ -10,23 +10,12 @@ function filePicker(filepickerService) {
         filepickerService.pick({
           accept: 'image/*',
           maxFiles: 1,
-<<<<<<< HEAD
           transformations: { crop: { force: true, aspectRatio: 3/2 } }
         }, data => {
           console.log(data);
           ngModel.$setViewValue(data.url);
         });
       });
-
-=======
-          transformation: { crop: { force: true, aspectRatio: 3/2 } }
-        }, data => {
-          console.log(data);
-          ngModel.$setViewValue(data.url);
-
-        });
-      });
->>>>>>> development
     }
   };
 }
