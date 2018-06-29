6d8627724f1216b0b5140c1682a5f16e788f798d
Rachel Dolan <racheldolan1@gmail.com>
2018-06-29 10:38:51 +0100

    Set up for deployment 🤞

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
-const port = 4000;
+const port = process.env.PORT || 4000;
 const env = process.env.NODE_ENV || 'dev';
-const dbURI = `mongodb://localhost/wdi-project-3${env}`;
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/wdi-project-3${env}`;
 const spoonKey = process.env.SPOONACULAR_API_KEY;
-const secret = 'P85t+sEHuX';
+const secret = process.env.SECRET || 'P85t+sEHuX';
 
 module.exports = { port, dbURI, spoonKey, secret };
